06e867d2a57ce804bdc8693c996b3b4a6791ac9a
kkindorf <kevin.kindorf@gmail.com>
2016-10-20 22:16:48 -0400

    added some styling and finished up calculator component

---
+++ components/percentButton.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+
+var PercentButton = React.createClass({
+  render: function(){
+    return(
+      <div>
+                <input type="button" value='%' onClick = {this.props.onClick} className="perc-button" />​
+      </div>
+    )
+  }
+})
+module.exports = PercentButton;
